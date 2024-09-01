8904fb40c2d2c9efec9309b553e3b05ac74f475d
ra6an <adnandacic145@gmail.com>
2024-09-01 21:48:34 +0200

    error handling

---
+++ API/Middleware/ExceptionsMiddleware.cs
@@ -0,0 +1,37 @@
+using System.Net;
+using System.Text.Json;
+using API.Errors;
+
+namespace API.Middleware
+{
+    public class ExceptionsMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<ExceptionsMiddleware> _logger;
+        private readonly IHostEnvironment _env;
+        public ExceptionsMiddleware(RequestDelegate next, ILogger<ExceptionsMiddleware> logger, IHostEnvironment env)
+        {
+            this._env = env;
+            this._logger = logger;
+            this._next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context) {
+            try {
+                await _next(context);
+            } catch (Exception e) {
+                _logger.LogError(e, e.Message);
+                context.Response.ContentType = "application/json";
+                context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+                var response = _env.IsDevelopment() ? new ApiException(context.Response.StatusCode, e.Message, e.StackTrace?.ToString()) : new ApiException(context.Response.StatusCode, e.Message, "Internal Server Error.");
+
+                var options = new JsonSerializerOptions{PropertyNamingPolicy = JsonNamingPolicy.CamelCase};
+
+                var json = JsonSerializer.Serialize(response, options);
+
+                await context.Response.WriteAsync(json);
+            }
+        }
+    }
+}
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -15,6 +15,8 @@ import { MemberDetailsComponent } from './members/member-details/member-details.
 import { ListsComponent } from './lists/lists.component';
 import { MessagesComponent } from './messages/messages.component';
 import { SharedModule } from './_modules/shared.module';
+import { TestErrorComponent } from './errors/test-error/test-error.component';
+import { ErrorInterceptor } from './_interceptors/error.interceptor';
 
 @NgModule({
   declarations: [
@@ -35,8 +37,11 @@ import { SharedModule } from './_modules/shared.module';
     BrowserAnimationsModule,
     FormsModule,
     SharedModule,
+    TestErrorComponent,
   ],
-  providers: [],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
