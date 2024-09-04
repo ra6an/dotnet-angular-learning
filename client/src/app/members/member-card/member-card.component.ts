89406248250e50a70df24f99256701e117c43b27
ra6an <adnandacic145@gmail.com>
2024-09-04 19:58:29 +0200

    members card second

--- client/src/app/members/member-card/member-card.component.ts
+++ client/src/app/members/member-card/member-card.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input, ViewEncapsulation } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { Member } from 'src/app/_models/member';
 
 @Component({
   selector: 'app-member-card',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './member-card.component.html',
   styleUrl: './member-card.component.css',
 })
--- client/src/app/members/member-details/member-details.component.ts
+++ client/src/app/members/member-details/member-details.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Member } from 'src/app/_models/member';
+import { MembersService } from 'src/app/_services/members.service';
 
 @Component({
   selector: 'app-member-details',
   templateUrl: './member-details.component.html',
-  styleUrls: ['./member-details.component.css']
+  styleUrls: ['./member-details.component.css'],
 })
 export class MemberDetailsComponent {
+  member: Member | undefined;
 
+  constructor(
+    private membersService: MembersService,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    this.loadMember();
+  }
+
+  loadMember() {
+    const username = this.route.snapshot.paramMap.get('username');
+
+    if (!username) return;
+
+    this.membersService.getMember(username).subscribe({
+      next: (m) => (this.member = m),
+    });
+  }
 }
